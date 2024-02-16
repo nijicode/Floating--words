410a1e4d9b7c5ffd13090cce3a017d6ee55b5210
nijicode <159203595+nijicode@users.noreply.github.com>
2024-02-16 19:15:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const words = [
+  "Hey",
+  "Hi",
+  "Hello",
+  "Miss na kita",
+  "Kamusta?",
+  "Pakyu",
+  "Bobo ka ba?",
+  "*",
+  "0",
+  "1",
+  "Magluluto lang ako saglet",
+];
+const container = document.querySelector(".container");
+
+function paulan() {
+  let drop = generateWordDrop();
+  container.appendChild(drop);
+}
+function pindutin() {
+  setInterval(paulan, 100);
+  removeDrop();
+}
+
+function removeDrop() {
+  setInterval(() => container.removeChild(container.firstChild), 150);
+}
+
+function generateRandom(mult, round) {
+  let result = Math.random() * mult;
+  if (round) {
+    result = Math.floor(result);
+  }
+  return result;
+}
+
+function generateWordDrop() {
+  let drop = document.createElement("span");
+  drop.classList.add("text");
+  drop.innerText = words[generateRandom(words.length, true)];
+  drop.style.left = `${20 + generateRandom(2500, true)}px`;
+  drop.style.fontSize = `${0.75 + generateRandom(1.5)}em`;
+  drop.style.animationDuration = `${1 + generateRandom(1)}s`;
+
+  return drop;
+}
